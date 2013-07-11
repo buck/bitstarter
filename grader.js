163d0fb4ce70c44ce56373a4035385489d8f06c8
A. Lester Buck III <buck@compact.com>
2013-07-11 16:19:15 -0500

    Working code, but lots of extra routines lying around.  Does not support exports yet.

--- grader.js
+++ grader.js
@@ -1,31 +1,12 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
 
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
-
-var fs = require('fs');
+var fs = require('fs'),
+    rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var page;  // the string that is the body of the web page
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,8 +17,23 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var loadUrl = function(url, callback) {
+    rest.get(url).on('complete', function(result) {
+      if (result instanceof Error) {
+	console.log('Error: ' + result.message);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+      } else {
+	callback(result);
+      }
+    });
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioUrlBody = function(urlbody) {
+    return cheerio.load(urlbody);
 };
 
 var loadChecks = function(checksfile) {
@@ -61,14 +57,46 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.parse(process.argv);
+var finishFile = function() {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+};
+
+var finishUrl = function() {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+var checkPageBody = function(page, checksfile) {
+    $ = cheerio.load(page);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+};
+
+var pageready = function(body) {
+    page = body;
+    checkPageBody(page, program.checks);
+};
+
+if(require.main == module) {
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url>', 'URL to index.html')
+	.parse(process.argv);
+    if (program.url) {
+        loadUrl(program.url, pageready);
+    } else {
+	pageready(fs.readFileSync(program.file));
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
